ff6ae3a1760c56a48be9fc05ac1624f38c700aad
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-01 21:41:24 +0900

    Update ReverseWord.java
    1st trial.

--- ReverseWord.java
+++ ReverseWord.java
@@ -5,29 +5,38 @@ public class ReverseWord {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
+        Boolean tag = false;
 		Stack<Character> st = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<s.length();i++) {
-			char c = s.charAt(i);
+        for(char ch:s.toCharArray()){
 			if(c == '<') {
-				while(c != '>') {
-					sb.append(c);
-				}
-				sb.append(c);
-			}
-			else if(c == '\n' || c == ' ') {//공백 문자거나 줄바꿈 문자이면 
-				while(!s.isEmpty()) {//스택이 빌 때까지 
+				while(!s.isEmpty()) {
 					System.out.println(st.pop());
+                    tag = true;
+                    System.out.println(c);
 				}
-			}else {//공백 문자/ 줄바꿈 문자 아니면 
-				st.push(c);//스택에 push.
 			}
-			st.push(c);
-			System.out.println("stack push :" +c);//h,e,u,n
-			sb.append(st.pop());//n u e h 가 되야하는데!!!!!
+			else if(c == '>') {//공백 문자거나 줄바꿈 문자이면 
+                tag = false;
+                System.out.println(c);
+			}else if(tag){//공백 문자/ 줄바꿈 문자 아니면 
+				System.out.println(c);
+			} else{
+                if(c == ' ') {
+                    while(!s.isEmpty()) {
+					System.out.println(st.pop());
+                    
+                    System.out.println(c);
+				} else {
+			        st.push(c);
+                }
+                }
 		
-			System.out.print(sb);
+	        }
+        }
+        while(!s.isEmpty()) {
+			System.out.println(st.pop());
+            System.out.println(c;
 		}
-	}
+        
 
 }
