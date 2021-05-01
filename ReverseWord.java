4dd65b99cd5ea85e1c602d4843936187be226d7f
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-01 21:44:28 +0900

    Update ReverseWord.java
    Change to using BufferedReader and BufferedWriter.
    Create print method and boolean variable to check wheter char is in tag or not.
    if char is '<', boolean is true and following chars are printed in turns.
    if char is '>', boolean is false.
    if char is out of tag(normal word), it calls print method and print reverse or push to stack.

--- ReverseWord.java
+++ ReverseWord.java
@@ -1,42 +1,42 @@
 package data_structures;
+
 import java.util.*;
+import java.io.*;
+
 public class ReverseWord {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-        Boolean tag = false;
-		Stack<Character> st = new Stack<>();
-        for(char ch:s.toCharArray()){
-			if(c == '<') {
-				while(!s.isEmpty()) {
-					System.out.println(st.pop());
-                    tag = true;
-                    System.out.println(c);
-				}
-			}
-			else if(c == '>') {//공백 문자거나 줄바꿈 문자이면 
-                tag = false;
-                System.out.println(c);
-			}else if(tag){//공백 문자/ 줄바꿈 문자 아니면 
-				System.out.println(c);
-			} else{
-                if(c == ' ') {
-                    while(!s.isEmpty()) {
-					System.out.println(st.pop());
-                    
-                    System.out.println(c);
-				} else {
-			        st.push(c);
-                }
-                }
-		
-	        }
+    static void print(BufferedWriter bw, Stack<Character> s) throws IOException {
+        while (!s.isEmpty()) {
+            bw.write(s.pop());
         }
-        while(!s.isEmpty()) {
-			System.out.println(st.pop());
-            System.out.println(c;
-		}
-        
-
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean tag = false;
+        Stack<Character> s = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '<') {
+                print(bw, s);
+                tag = true;
+                bw.write(ch);//< 출력
+            } else if (ch == '>') {
+                tag = false;
+                bw.write(ch);//> 출력
+            } else if (tag) {
+                bw.write(ch);
+            } else {//단어뒤집기1과 동일.
+                if (ch == ' ') {
+                    print(bw, s);
+                    bw.write(ch);
+                } else {
+                    s.push(ch);
+                }
+            }
+        }
+        print(bw, s);//혹시 스택에 남아있는 것이 있다면 다 출력.
+        bw.write("\n");
+        bw.flush();
+    }
 }
+
