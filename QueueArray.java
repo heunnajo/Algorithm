299482c516349e79b62338212ff493fd15ed83d3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-01 17:36:16 +0900

    Update QueueArray.java
    Delete unnecessary code including comments.

--- QueueArray.java
+++ QueueArray.java
@@ -7,20 +7,16 @@ public class Queue2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		sc.nextLine();//개행문자 제거하는 목적!
-		int[] queue = new int[n];//정적  자료구조 배열 크기 어떻게 잡아야 하나!
+		int[] queue = new int[n];
 		int begin = 0, end = 0;
 		
 		while(n-- >0) {
-			//명령어  입력받는따!
 			
-			String line = sc.nextLine();
-			String[] str = line.split(" ");//공백문자로 구분한다.
-			String cmd = str[0];
+            String cmd = sc.next();
 			if(cmd.equals("push")) {
-				int num = Integer.parseInt(str[1]);
-				queue[end] = num;
-				end++;
+				int num = Integer.parseInt(sc.next());
+                queue[end++] = num;
+				
 			} else if(cmd.equals("pop")) {
 				if(begin == end) System.out.println("-1");
 				else {
@@ -34,9 +30,11 @@ public class Queue2 {
 				if(begin == end) System.out.println("1");
 				else System.out.println("0");
 			} else if(cmd.equals("front")) {
-				System.out.println(queue[begin]);
+                if(begin == end) {System.out.println("-1");}//예외처리
+				else {System.out.println(queue[begin]);}
 			} else if(cmd.equals("back")) {
-				System.out.println(queue[end-1]);
+                if(begin == end) {System.out.println("-1");}//예외처리
+				else {System.out.println(queue[end-1]);}
 			}
 		}
 	
