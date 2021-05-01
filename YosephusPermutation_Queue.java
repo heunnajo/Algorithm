99dcb502404978271bc502c7f34ecf39d0fb785f
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-01 18:27:46 +0900

    Update YosephusPermutation_Queue.java
    ADD System.out.println(sb)!!

--- YosephusPermutation_Queue.java
+++ YosephusPermutation_Queue.java
@@ -15,13 +15,14 @@ public class Josephus {
 			myq.offer(i);
 		}
 		sb.append("<");
-		for(int j=0;j<m-2;j++) {//j=0,1,2,3,4,5,6 총 7번 돈다.
+		for(int j=0;j<m-1;j++) {//마지막 원소 하나 남겨두고 이중 for문 종료 
 			for(int k=0;k<n-1;k++) {
 				myq.offer(myq.poll());
 			}
 			sb.append(myq.poll()+",");//3,6,2,7,5,1,
 		}
 		sb.append(myq.poll()+">\n");
+        System.out.println(sb);
 	}
 
 }
