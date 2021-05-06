ab0ed8995ac6928953d58acf8dbf6533f6361d8a
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-06 17:00:36 +0900

    Create GCDSum.java

---
+++ GCDSum.java
@@ -0,0 +1,31 @@
+package data_structures;
+import java.util.*;
+
+public class GCDSum {
+
+	static int GCD(int a,int b) {
+		if(b == 0) return a;
+		else {
+			return GCD(b,a%b);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-- >0) {
+			int n = sc.nextInt();
+			int[] nums = new int[n];
+			for(int i=0;i<n;i++) {
+				nums[i] = sc.nextInt();
+			}
+			long sum = 0;
+			for(int i=0;i<n-1;i++) {
+				for(int j=i+1;j<n;j++) {
+					sum += GCD(nums[i],nums[j]);
+				}
+			}
+			System.out.println(sum);
+		}
+	}
+
+}
