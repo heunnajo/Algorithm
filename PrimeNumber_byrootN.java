9a0e80c9226f08164f4cea1ee6d43d81eb347885
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-07 12:08:42 +0900

    Create PrimeNumber_byrootN.java
    Solved Finding Prime Number probelm by using root of prime number traits. Its time complexity is less than given time constraint. So there's no need to use Sieve of Eratosthenese algorithm.

---
+++ PrimeNumber_byrootN.java
@@ -0,0 +1,30 @@
+package data_structures;
+import java.util.*;
+
+public class PrimeNumber_byrootN {
+	public static boolean is_prime(int x) {
+        if (x <= 1) {
+            return false;
+        } else if (x == 2) {
+            return true;
+        }
+        for (int i=2; i*i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n-- > 0) {
+            if (is_prime(sc.nextInt())) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+	}
+
+}
