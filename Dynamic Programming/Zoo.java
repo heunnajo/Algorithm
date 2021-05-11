7b24691f8e48ee2a57fe3a1ddf3b1c0fc399def6
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-05-11 11:07:19 +0900

    Update Zoo.java

--- Dynamic Programming/Zoo.java
+++ Dynamic Programming/Zoo.java
@@ -5,18 +5,17 @@ public class Zoo {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[][] d = new int [n+1][3];
-		int mod = 9901;
-		for(int i=0;i<3;i++) {
-			d[0][i] = 1;
-		}
+		d[0][0] = 1;
 		for(int i=1;i<=n;i++) {
 			d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2];
 			d[i][1] = d[i-1][0] + d[i-1][2];
 			d[i][2] = d[i-1][0] + d[i-1][1];
-				
+			for(int j=0;j<3;j++){
+                d[i][j] %= 9901;
+            }
 		}
 		int ans = 0;
-		ans = (d[n][0] + d[n][1] + d[n][2])%mod;
+		ans = (d[n][0] + d[n][1] + d[n][2])%9901;
 		
 		System.out.println(ans);
 	}
